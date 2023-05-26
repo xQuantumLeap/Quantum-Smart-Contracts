d921e5c6385cf34fac01d3ee693388d259c82167
xQuantumLeap <133878080+xQuantumLeap@users.noreply.github.com>
2023-05-26 23:48:17 +0300

    Update quantom_token_erc20.sol

--- quantom_token_erc20.sol
+++ quantom_token_erc20.sol
@@ -6,36 +6,11 @@
 
 pragma solidity ^0.8.9;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-contract QuantumToken is ERC20, Ownable {
+import "@openzeppelin/contracts@4.9.0/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts@4.9.0/access/Ownable.sol";
 
-    address public treasury;
-    uint256 public transactionFeePercentage = 5; 
-
-    constructor(string memory name, string memory symbol, uint8 decimals, uint256 totalSupply) ERC20(name, symbol) {
-        _mint(msg.sender, totalSupply);
-        treasury = 0x265642e14424236b4848c61Bff2F631B1EFDeE84;
-    }
-
-    function transfer(address recipient, uint256 amount) public virtual override returns (bool) {
-        uint256 fee = calculateTransactionFee(amount);
-        uint256 valueAfterFee = amount - fee;
-        _transfer(msg.sender, recipient, valueAfterFee);
-        _transfer(msg.sender, treasury, fee);
-        return true;
-    }
-
-    function transferFrom(address sender, address recipient, uint256 amount) public virtual override returns (bool) {
-        uint256 fee = calculateTransactionFee(amount);
-        uint256 valueAfterFee = amount - fee;
-        _transfer(sender, recipient, valueAfterFee);
-        _transfer(sender, treasury, fee);
-        _approve(sender, msg.sender, allowance(sender, msg.sender) - amount + valueAfterFee);
-        return true;
-    }
-
-    function calculateTransactionFee(uint256 amount) internal view returns (uint256) {
-        return (amount * transactionFeePercentage) / 100;
+contract Quantum is ERC20, Ownable {
+    constructor() ERC20("Quantum", "QLT") {
+        _mint(msg.sender, 100000000 * 10 ** decimals());
     }
 }
